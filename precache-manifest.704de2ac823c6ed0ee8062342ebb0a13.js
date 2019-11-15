063713be65ae03b0d0cc182bbd65f147ad259953
Edward Adam Farris <edwardfarris81@mail.com>
2019-11-15 10:58:36 -0700

    Updates

---
+++ precache-manifest.704de2ac823c6ed0ee8062342ebb0a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313f705e2dcf261c03dec55c8db3f99f",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "90809259414b20161161",
+    "url": "/jammming/static/css/main.2f5afcca.chunk.css"
+  },
+  {
+    "revision": "40828de45c9cc3153e0c",
+    "url": "/jammming/static/js/2.1f2a172f.chunk.js"
+  },
+  {
+    "revision": "90809259414b20161161",
+    "url": "/jammming/static/js/main.f8536115.chunk.js"
+  },
+  {
+    "revision": "baf9ba99f56cb6195e90",
+    "url": "/jammming/static/js/runtime-main.63473824.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
